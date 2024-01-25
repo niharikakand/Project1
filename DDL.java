6bf8babf70401b827664685a0abe19cf2362f896
nihar <niharikakandepu57@gmail.com>
2024-01-25 13:30:58 -0500

    new1

--- DDL.java
+++ DDL.java
@@ -1,4 +1,40 @@
-public class DDL {
-System.out.println("Hello");
+public class DLL<E> {
+ private static class DLL<E> {
+    //private element <E>;
+     private Node<E> head;
+     private Node<E> tail;
+     private int counter;
 
-}
+
+ } // static class
+public DLL() {
+this.counter = 0 ;
+this.head= 0;
+this.tail=0;
+    }
+public int size() {
+     return size;
+} //size
+public boolean isEmpty() {
+if(this.size = 0 ) {
+reutrn true;
+} else {
+    return false;
+} // else
+
+}// isEmpty
+
+ public E first(){
+
+ } //E First
+
+public E last() {
+
+  } // Elast
+
+public void addFrist() {
+
+
+} //addFrist
+
+} // DDL class
